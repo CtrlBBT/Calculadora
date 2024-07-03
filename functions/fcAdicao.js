1b87b4ac051b7ba1867adaca4d3d91910a6d8f93
CtrlBBT <ctrlmarkbbt@gmail.com>
2024-07-03 19:55:44 -0300

    ARQUIVOS CALCULADORA

---
+++ functions/fcAdicao.js
@@ -0,0 +1,12 @@
+function adicao(numero1, numero2) {
+    if (numero1 != Number | numero2 != Number) {
+        let numero1 = prompt("Digite números válidos. Digite o Primeiro Número para a Conta: ");
+        let numero2 = prompt("Digite o Segundo Número para a Conta: ");
+    } else {
+        let soma = numero1 + numero2        
+
+        return soma
+    }
+}
+
+module.exports = soma;
---
+++ index.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+console.log("Vamos fazer cálculos!")
+
+let numero1 = prompt("Digite o primeiro Número para a Conta: ");
+let numero2 = prompt("Digite o segundo Número para a Conta: ");
+
+let conta = prompt("Qual conta iremos realizar? Digite o Número correspondente ao cálculo:\n\ 1. Adição\n\ 2. Subtração\n\ 3. Multiplicação\n\ 4. Divisão\n\ 5. Porcentagem");
