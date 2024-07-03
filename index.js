f3c8c9559b69d20cfa49b1dd91e543a095b15f0b
CtrlBBT <ctrlmarkbbt@gmail.com>
2024-07-03 20:40:06 -0300

    correções de visual

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ let num2 = parseFloat(prompt("Digite o segundo número:"));
 let conta = prompt("Qual conta iremos realizar? Digite o Número correspondente ao cálculo:\n\ 1. Adição\n\ 2. Subtração\n\ 3. Multiplicação\n\ 4. Divisão\n\ 5. Porcentagem");
 
 if (!isNaN(num1) && !isNaN(num2)) {
-    if (conta === 1) {
-      const soma = require('./functions/fcAdicao')
+      if (conta === 1) {
+        const soma = require('./functions/fcAdicao')
     } if (conta === 2) {
         const subtracao = require('./functions/fcSubtracao')
     } if (conta === 3) {
         const multiplicacao = require('./functions/fcMultiplicacao')
-    }if (conta === 4) {
+    } if (conta === 4) {
         const divisao = require('./functions/fcDivisao')
-    }if (conta === 5) {
+    } if (conta === 5) {
         const porcentagem = require('./functions/fcPorcentagem')
     }
 
